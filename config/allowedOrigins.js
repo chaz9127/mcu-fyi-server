c82f606e3d5fd92ef17ba8bf92a7f23efd894cf5
CJ Douglas <cj@lola.com>
2024-02-22 21:26:34 -0500

    allow render.com to access backend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'https://www.themcu.fyi',
   'https://themcu.fyi',
+  'https://www.mcu-fyi-client.onrender.com',
+  'https://mcu-fyi-client.onrender.com'
 ]
 
 module.exports = allowedOrigins;
